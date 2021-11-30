44a2d0e8271d37b0edf74584e06af995058cea6e
Ramazan Akbal <43077625+Andromeda606@users.noreply.github.com>
2021-11-30 14:40:40 +0300

    Update RequestNetworkController.java

--- requestnetwork/src/main/java/com/andromeda/requestnetwork/request/RequestNetworkController.java
+++ requestnetwork/src/main/java/com/andromeda/requestnetwork/request/RequestNetworkController.java
@@ -1,69 +1,230 @@
 package com.andromeda.requestnetwork.request;
 
-import android.app.Activity;
+import android.annotation.SuppressLint;
 
-import com.google.gson.reflect.TypeToken;
+import androidx.annotation.NonNull;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.security.cert.CertificateException;
 import java.util.HashMap;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.Headers;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class RequestNetwork {
-    private HashMap<String, Object> params = new HashMap<>();
-    private String paramsJson = null;
-    private HashMap<String, Object> headers = new HashMap<>();
+public class RequestNetworkController {
+    public static final String GET = "GET";
+    public static final String POST = "POST";
+    public static final String PUT = "PUT";
+    public static final String DELETE = "DELETE";
 
-    private final Activity activity;
+    private static final int SOCKET_TIMEOUT = 15000;
+    private static final int READ_TIMEOUT = 25000;
 
-    public RequestNetwork(Activity activity) {
-        this.activity = activity;
+    protected OkHttpClient client;
+
+    private static RequestNetworkController mInstance;
+
+    public static synchronized RequestNetworkController getInstance() {
+        if (mInstance == null) {
+            mInstance = new RequestNetworkController();
+        }
+        return mInstance;
     }
 
-    public void setHeaders(HashMap<String, Object> headers) {
-        this.headers = headers;
+    @SuppressLint("CustomX509TrustManager")
+    private OkHttpClient getClient() {
+        if (client == null) {
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+
+            try {
+                final TrustManager[] trustAllCerts = new TrustManager[]{
+
+                        new X509TrustManager() {
+                            @SuppressLint("TrustAllX509TrustManager")
+                            @Override
+                            public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                            }
+
+                            @SuppressLint("TrustAllX509TrustManager")
+                            @Override
+                            public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                            }
+
+                            @Override
+                            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                                return new java.security.cert.X509Certificate[]{};
+                            }
+                        }
+                };
+
+                final SSLContext sslContext = SSLContext.getInstance("TLS");
+                sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+                final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+                builder.sslSocketFactory(sslSocketFactory, (X509TrustManager) trustAllCerts[0]);
+                builder.connectTimeout(SOCKET_TIMEOUT, TimeUnit.MILLISECONDS);
+                builder.readTimeout(READ_TIMEOUT, TimeUnit.MILLISECONDS);
+                builder.writeTimeout(READ_TIMEOUT, TimeUnit.MILLISECONDS);
+                builder.hostnameVerifier((hostname, session) -> true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            client = builder.build();
+        }
+
+        return client;
     }
 
-    public void setParams(HashMap<String, Object> params) {
-        this.params = params;
+
+    public void execute(RequestNetwork requestNetwork, String method, String url, RequestNetwork.RequestListener requestListener) {
+        try {
+
+            init(requestNetwork, method, url).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull final IOException e) {
+                    if (requestNetwork.getActivity() == null) {
+                        requestListener.onErrorResponse(e.getMessage());
+                    } else {
+                        requestNetwork.getActivity().runOnUiThread(() -> requestListener.onErrorResponse(e.getMessage()));
+                    }
+
+                }
+
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull final Response response) throws IOException {
+                    assert response.body() != null;
+                    final String responseBody = response.body().string().trim();
+                    if (requestNetwork.getActivity() == null) {
+                        requestListener.onResponse(responseBody, response);
+                    } else {
+                        requestNetwork.getActivity().runOnUiThread(() -> {
+                            requestListener.onResponse(responseBody, response);
+                        });
+                    }
+                }
+            });
+        } catch (Exception e) {
+            requestListener.onErrorResponse(e.getMessage());
+        }
     }
 
-    public void setParams(String params) {
-        this.paramsJson = params;
+    public <D> void executee(RequestNetwork requestNetwork, String method, String url, RequestNetwork.GsonRequestListener<D> requestListener, Type type) {
+        try {
+
+            init(requestNetwork, method, url).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull final IOException e) {
+                    if (requestNetwork.getActivity() == null) {
+                        requestListener.onErrorResponse(e.getMessage());
+                    } else {
+                        requestNetwork.getActivity().runOnUiThread(() -> requestListener.onErrorResponse(e.getMessage()));
+                    }
+
+                }
+
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull final Response response) throws IOException {
+                    assert response.body() != null;
+                    final String responseBody = response.body().string().trim();
+                    if (requestNetwork.getActivity() == null) {
+                        try {
+                            D data = new Gson().fromJson(responseBody, type);
+                            requestListener.onResponse(responseBody, response, data);
+                        }catch (Exception exception){
+                            requestListener.onResponse(responseBody, response, null);
+                        }
+                    } else {
+                        requestNetwork.getActivity().runOnUiThread(() -> {
+                            try {
+                                D data = new Gson().fromJson(responseBody, type);
+                                requestListener.onResponse(responseBody, response, data);
+                            }catch (Exception exception){
+                                requestListener.onResponse(responseBody, response, null);
+                            }
+
+
+                        });
+                    }
+                }
+            });
+        } catch (Exception e) {
+            requestListener.onErrorResponse(e.getMessage());
+        }
     }
 
-    public String getParamsJson(){
-        return paramsJson;
-    }
+    private Call init(RequestNetwork requestNetwork, String method, String url) {
+        Request.Builder reqBuilder = new Request.Builder();
+        Headers.Builder headerBuilder = new Headers.Builder();
 
-    public HashMap<String, Object> getParams() {
-        return params;
-    }
+        if (requestNetwork.getHeaders().size() > 0) {
+            HashMap<String, Object> headers = requestNetwork.getHeaders();
 
-    public HashMap<String, Object> getHeaders() {
-        return headers;
-    }
+            for (HashMap.Entry<String, Object> header : headers.entrySet()) {
+                headerBuilder.add(header.getKey(), String.valueOf(header.getValue()));
+            }
+        }
 
-    public Activity getActivity() {
-        return activity;
-    }
+        if (requestNetwork.getParamsJson() != null) {
+            RequestBody reqBody = RequestBody.create(okhttp3.MediaType.parse("application/json"), requestNetwork.getParamsJson());
 
-    public void startRequestNetwork(String method, String url, RequestListener requestListener) {
-        RequestNetworkController.getInstance().execute(this, method, url, requestListener);
-    }
+            if (method.equals(GET)) {
+                reqBuilder.url(url).headers(headerBuilder.build()).get();
+            } else {
+                reqBuilder.url(url).headers(headerBuilder.build()).method(method, reqBody);
+            }
+        } else {
+            if (method.equals(GET)) {
+                HttpUrl.Builder httpBuilder;
 
-    public <D> void startRequestNetworkGson(String method, String url, GsonRequestListener<D> requestListener) {
-        RequestNetworkController.getInstance().executee(this, method, url, requestListener, new TypeToken<D>(){}.getType());
-    }
+                try {
+                    httpBuilder = Objects.requireNonNull(HttpUrl.parse(url)).newBuilder();
+                } catch (NullPointerException ne) {
+                    throw new NullPointerException("unexpected url: " + url);
+                }
 
+                if (requestNetwork.getParams().size() > 0) {
+                    HashMap<String, Object> params = requestNetwork.getParams();
 
-    public interface RequestListener {
-        void onResponse(String response, Response request);
-        void onErrorResponse(String message);
-    }
+                    for (HashMap.Entry<String, Object> param : params.entrySet()) {
+                        httpBuilder.addQueryParameter(param.getKey(), String.valueOf(param.getValue()));
+                    }
+                }
 
-    public interface GsonRequestListener<D> {
-        void onResponse(String response, Response request, D gson);
-        void onErrorResponse(String message);
+                reqBuilder.url(httpBuilder.build()).headers(headerBuilder.build()).get();
+            } else {
+                FormBody.Builder formBuilder = new FormBody.Builder();
+                if (requestNetwork.getParams().size() > 0) {
+                    HashMap<String, Object> params = requestNetwork.getParams();
+
+                    for (HashMap.Entry<String, Object> param : params.entrySet()) {
+                        formBuilder.add(param.getKey(), String.valueOf(param.getValue()));
+                    }
+                }
+
+                RequestBody reqBody = formBuilder.build();
+
+                reqBuilder.url(url).headers(headerBuilder.build()).method(method, reqBody);
+            }
+        }
+
+        Request req = reqBuilder.build();
+        return getClient().newCall(req);
     }
 }
